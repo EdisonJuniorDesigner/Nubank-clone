9d7409b02158c7d0a44253ae27b66a6d62c06caa
EdisonJuniorDesigner <mage1232010@gmail.com>
2019-09-19 19:18:38 -0300

    First Commit

---
+++ src/components/Menu/index.js
@@ -0,0 +1,55 @@
+import React from 'react';
+
+import QRCode from '~/assets/QRCode.png';
+
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import {
+  Container,
+  Code,
+  QRCodeS,
+  Nav,
+  NavItem,
+  NavText,
+  SignOutButton,
+  SignOutButtonText,
+} from './styles';
+
+export default function Menu({translateY}) {
+  return (
+    <Container
+      style={{
+        opacity: translateY.interpolate({
+          inputRange: [0, 150],
+          outputRange: [0, 1],
+        }),
+      }}>
+      <Code>
+        <QRCodeS source={QRCode} />
+      </Code>
+
+      <Nav>
+        <NavItem>
+          <Icon name="help-outline" size={20} color="#fff" />
+          <NavText>Me ajuda</NavText>
+        </NavItem>
+        <NavItem>
+          <Icon name="person-outline" size={20} color="#fff" />
+          <NavText>Perfil</NavText>
+        </NavItem>
+        <NavItem>
+          <Icon name="credit-card" size={20} color="#fff" />
+          <NavText>Configurar cartão</NavText>
+        </NavItem>
+        <NavItem>
+          <Icon name="smartphone" size={20} color="#fff" />
+          <NavText>Configurações do app</NavText>
+        </NavItem>
+      </Nav>
+
+      <SignOutButton onPress={() => {}}>
+        <SignOutButtonText>SAIR DO APP</SignOutButtonText>
+      </SignOutButton>
+    </Container>
+  );
+}
